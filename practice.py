b9b4949a5cf08078791cb97b8c8be121392c2cb6
aladiev.id <aladiev.id@barl.ru>
2020-12-19 14:29:32 +0300

    Refactor

--- practice.py
+++ practice.py
@@ -5,8 +5,13 @@ from tkinter import filedialog as fd
 from PIL import Image, ImageFilter, ImageTk
 import math
 
+radius = 3
+width = 1700
+height = 700
+
 class MyPolygon(object):
     def __init__(self, points, points_ids, canvas):
+        self.point_radius = radius
         self.canvas = canvas
 
         self.points = points.copy()
@@ -50,10 +55,10 @@ class MyPolygon(object):
 
         for point_id in range(self.count):
             self.points_ids.append(self.canvas.create_oval(
-                self.points[point_id][0] - 10,
-                self.points[point_id][1] - 10,
-                self.points[point_id][0] + 10,
-                self.points[point_id][1] + 10,
+                self.points[point_id][0] - self.point_radius,
+                self.points[point_id][1] - self.point_radius,
+                self.points[point_id][0] + self.point_radius,
+                self.points[point_id][1] + self.point_radius,
                 outline="#0000FF",
                 fill="#0000FF",
                 tags=("point")
@@ -67,8 +72,8 @@ class MyPolygon(object):
 
     def is_intersection(self, point):
         for i in range(self.count):
-            x_condition = self.points[i][0] - 10 <= point[0] and self.points[i][0] + 10 >= point[0]
-            y_condition = self.points[i][1] - 10 <= point[1] and self.points[i][1] + 10 >= point[1]
+            x_condition = self.points[i][0] - self.point_radius <= point[0] and self.points[i][0] + self.point_radius >= point[0]
+            y_condition = self.points[i][1] - self.point_radius <= point[1] and self.points[i][1] + self.point_radius >= point[1]
 
             if x_condition and y_condition:
                 return i
@@ -83,6 +88,7 @@ class MyPolygon(object):
             return
 
         new_points = [self.points[0]]
+        new_initial_points = [self.initial_points[0]]
 
         for i in range(len(self.points) - 1):
             x_offset = (self.points[i + 1][0] - self.points[i][0]) * scale / self.scale
@@ -90,8 +96,18 @@ class MyPolygon(object):
 
             new_points.append([new_points[i][0] + x_offset, new_points[i][1] + y_offset])
 
+
+        for i in range(len(self.initial_points) - 1):
+            x_offset = (self.initial_points[i + 1][0] - self.initial_points[i][0]) * scale / self.scale
+            y_offset = (self.initial_points[i + 1][1] - self.initial_points[i][1]) * scale / self.scale
+
+            new_initial_points.append([new_initial_points[i][0] + x_offset, new_initial_points[i][1] + y_offset])
+
+        self.initial_points = new_initial_points
         self.points = new_points
         self.scale = scale
+
+        self.calculate_center()
 
         self.draw_points()
         self.draw_lines()
@@ -120,14 +136,19 @@ class MyPolygon(object):
 
 class Example(Frame):
     def __init__(self, parent):
+        self.bg_id = -1
+        self.point_radius = radius
+
         Frame.__init__(self, parent)
 
-        self.poly = None
-        self.is_created_poly = False
+        self.polygons = []
+
+        # True - Кликаем точки А и Б, False - Создаем полигон
+        self.mode = False
         self.points = []
         self.points_ids = []
 
-        self.canvas = Canvas(width=1280, height=520, background="bisque")
+        self.canvas = Canvas(width=width, height=height, background="bisque")
         self.canvas.pack(fill="both", expand=True)
 
         self.scale = Scale(parent, digits=3,command=self.change_scale, orient=HORIZONTAL, length=1000, from_=0.25, to=2,
@@ -136,69 +157,82 @@ class Example(Frame):
         self.rotation = Scale(parent, digits=3, command=self.rotate, orient=HORIZONTAL, length=1000, from_=-180,
                            to=180,
                            tickinterval=30, resolution=1, label="Поворот")
+
+        self.entry = Entry(parent, width=20,bd=3)
+
         self.loading = Button(parent, text="Import Polygon", command= self.import_poly)
         self.loadImage = Button(parent, text="Open IMG", command= self.openImage)
+
+        self.entry.pack()
+        self.scale.pack()
+        self.rotation.pack()
         self.loading.pack()
         self.loadImage.pack()
 
-        self._drag_data = {"x": 0, "y": 0, "item": None, "id": -1, "is_poly": False}
+        self._drag_data = {"x": 0, "y": 0, "item": None, "id": -1, "is_poly": False, "poly_id": -1}
 
         self.canvas.tag_bind("point", "<ButtonPress-3>", self.drag_start)
         self.canvas.tag_bind("point", "<ButtonRelease-3>", self.drag_stop)
         self.canvas.tag_bind("point", "<B3-Motion>", self.drag)
-        self.canvas.tag_bind("point", "<ButtonPress-2>", self.create_poly)
+        self.canvas.bind("<ButtonPress-2>", self.finish_poly)
         self.canvas.bind("<Button-1>", self.create_point)
+
+    def finish_poly(self, event):
+        if len(self.points) > 2:
+            self.create_poly(self.points, self.points_ids)
+
+            self.points = []
+            self.points_ids = []
 
     def create_point(self, event):
         """Create a token at the given coordinate in the given color"""
-        print("Created!")
-        color = "#0000FF"
-
-        if self.is_created_poly:
-            color = "#00FF00" if self.localization([event.x, event.y]) else "#FF0000"
+        color = "#00FF00" if self.mode else "#0000FF"
 
         self.points.append([event.x, event.y, color])
         self.points_ids.append(self.canvas.create_oval(
-            event.x - 10,
-            event.y - 10,
-            event.x + 10,
-            event.y + 10,
+            event.x - self.point_radius,
+            event.y - self.point_radius,
+            event.x + self.point_radius,
+            event.y + self.point_radius,
             outline=color,
             fill=color,
             tags=("point")
         ))
 
-    def create_poly(self, event):
-        if not self.is_created_poly:
-            self.is_created_poly = True
+    def create_poly(self, points, points_ids):
+        if len(points) > 2:
+            current_poly = MyPolygon(points, points_ids, self.canvas)
 
-            self.poly = MyPolygon(self.points, self.points_ids, self.canvas)
-
-            self.points = []
-            self.points_ids = []
-
-            self.scale.pack()
-            self.rotation.pack()
+            self.polygons.append(current_poly)
 
     def drag_start(self, event):
         """Begining drag of an object"""
         # record the item and its location
-        self._drag_data["item"] = self.canvas.find_closest(event.x, event.y)[0]
-        self._drag_data["x"] = event.x
-        self._drag_data["y"] = event.y
+        id = self.canvas.find_closest(event.x, event.y)[0]
 
-        if self.is_intersection([event.x, event.y]) != -1:
-            self._drag_data["id"] = self.is_intersection([event.x, event.y])
-            self._drag_data["is_poly"] = False
-        else:
-            self._drag_data["is_poly"] = True
-            self._drag_data["id"] = self.poly.is_intersection([event.x, event.y])
+        if self.bg_id != id:
+            self._drag_data["item"] = id
+            self._drag_data["x"] = event.x
+            self._drag_data["y"] = event.y
+
+            if self.is_intersection([event.x, event.y]) != -1:
+                self._drag_data["id"] = self.is_intersection([event.x, event.y])
+                self._drag_data["is_poly"] = False
+            else:
+                self._drag_data["is_poly"] = True
+                for poly_id in range(len(self.polygons)):
+                    point_id = self.polygons[poly_id].is_intersection([event.x, event.y])
+
+                    if point_id > -1:
+                        self._drag_data["poly_id"] = poly_id
+                        self._drag_data["id"] = point_id
+                        break
 
     def drag_stop(self, event):
         """End drag of an object"""
         # reset the drag information
         if self._drag_data["is_poly"]:
-            self.poly.set_vertex(self._drag_data["id"], [event.x, event.y])
+            self.polygons[self._drag_data["poly_id"]].set_vertex(self._drag_data["id"], [event.x, event.y])
         else:
             self.points[self._drag_data["id"]][0] = event.x
             self.points[self._drag_data["id"]][1] = event.y
@@ -208,8 +242,7 @@ class Example(Frame):
         self._drag_data["x"] = 0
         self._drag_data["y"] = 0
         self._drag_data["is_poly"] = False
-
-        self.recalculate_colors()
+        self._drag_data["poly_id"] = -1
 
     def drag(self, event):
         """Handle dragging of an object"""
@@ -223,119 +256,67 @@ class Example(Frame):
         self._drag_data["y"] = event.y
 
         if self._drag_data["is_poly"]:
-            self.poly.get_points()[self._drag_data["id"]] = [event.x, event.y]
-            self.poly.draw_lines()
+            self.polygons[self._drag_data["poly_id"]].get_points()[self._drag_data["id"]] = [event.x, event.y]
+            self.polygons[self._drag_data["poly_id"]].draw_lines()
         else:
             self.points[self._drag_data["id"]][0] = event.x
             self.points[self._drag_data["id"]][1] = event.y
 
+    # TODO: Refactor?
     def is_intersection(self, point):
         for i in range(len(self.points)):
-            x_condition = self.points[i][0] - 10 <= point[0] and self.points[i][0] + 10 >= point[0]
-            y_condition = self.points[i][1] - 10 <= point[1] and self.points[i][1] + 10 >= point[1]
+            x_condition = self.points[i][0] - self.point_radius <= point[0] and self.points[i][0] + self.point_radius >= point[0]
+            y_condition = self.points[i][1] - self.point_radius <= point[1] and self.points[i][1] + self.point_radius >= point[1]
 
             if x_condition and y_condition:
                 return i
 
         return -1
 
-    def localization(self, point):
-        x = point[0]
-        y = point[1]
-
-        neighbors = []
-
-        vertexes = self.poly.get_points()
-
-        count = len(vertexes)
-
-        for i in range(count):
-            x1 = vertexes[i][0]
-            x2 = vertexes[(i + 1) % count][0]
-            y1 = vertexes[i][1]
-            y2 = vertexes[(i + 1) % count][1]
-
-            if y2 > y and y1 <= y or y1 > y and y2 <= y:
-                neighbors.append(((x1-x2) * y + x2 * y1 - x1 * y2)/(y1 - y2))
-
-        if len(neighbors) < 2:
-            return False
-        else:
-            left = []
-            right = []
-
-            for i in range(len(neighbors)):
-                if x > neighbors[i]:
-                    left.append(neighbors[i])
-                elif x < neighbors[i]:
-                    right.append(neighbors[i])
-
-            if len(left) == len(right):
-                if len(left) % 2:
-                    return True
-                else:
-                    return False
-            else:
-                if len(left) % 2:
-                    return True
-
-                return False
-
-    def change_color(self, id, color):
-        self.canvas.itemconfig(id, fill=color, outline=color)
-
-    def recalculate_colors(self):
-        for i in range(len(self.points)):
-            predicted_color = "#00FF00" if self.localization(self.points[i]) else "#FF0000"
-
-            if self.points[i][2] != predicted_color:
-                self.change_color(self.points_ids[i], predicted_color)
-                self.points[i][2] = predicted_color
-
     def change_scale(self, event):
-        self.poly.set_scale(float(event))
-
-        self.recalculate_colors()
+        if self.entry.get():
+            self.polygons[int(self.entry.get())].set_scale(float(event))
 
     def rotate(self, event):
-        self.poly.rotation(math.pi * int(event) / 180)
-
-        self.recalculate_colors()
+        if self.entry.get():
+            self.polygons[int(self.entry.get())].rotation(math.pi * int(event) / 180)
 
     def openImage(self):
         path = fd.askopenfilename()
  
         openUserImage = Image.open(path)
- 
-        print(openUserImage.size)
      
         userImage = ImageTk.PhotoImage(openUserImage)
      
         self.canvas.image = userImage
-        self.canvas.create_image(375, 275, image=userImage)
+        self.bg_id = self.canvas.create_image(width // 2, height // 2, image=userImage, tags=["image"])
+        self.canvas.tag_lower("image")
 
 
     def import_poly(self):
         color = "#0000FF"
+
         file_name = fd.askopenfilename()
         tree = ET.ElementTree(file=file_name)
-        root = tree.getroot()
-        for child_of_root in root:
-            for plygons in child_of_root:
-                self.points.append([int(plygons.attrib['x']), int(plygons.attrib['y']), color])
-                self.points_ids.append(self.canvas.create_oval(
-                    int(plygons.attrib['x']) - 10,
-                    int(plygons.attrib['y']) - 10,
-                    int(plygons.attrib['x']) + 10,
-                    int(plygons.attrib['y']) + 10,
+
+        for polygon in tree.getroot():
+            points = []
+            points_ids = []
+            for point in polygon:
+                points.append([int(point.attrib['x']), int(point.attrib['y']), color])
+                points_ids.append(self.canvas.create_oval(
+                    int(point.attrib['x']) - self.point_radius,
+                    int(point.attrib['y']) - self.point_radius,
+                    int(point.attrib['x']) + self.point_radius,
+                    int(point.attrib['y']) + self.point_radius,
                     outline=color,
                     fill=color,
                     tags=("point")
                 ))
-            self.poly = MyPolygon(self.points, self.points_ids, self.canvas)
+
+            self.create_poly(points, points_ids)
 
 if __name__ == "__main__":
     root = Tk()
     Example(root).pack(fill="both", expand=True)
     root.mainloop()
-    
